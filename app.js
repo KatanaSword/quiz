8ed5d9385c79d9de084acf92e603951076e32b1f
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2023-12-04 14:57:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const readlineSync = require("readline-sync");
+const chalk = require("chalk");
+const log = console.log;
+const gkQuiz = require("./dataBase");
+
+let score = 0;
+let userName;
+let options = ["a", "b", "c", "d"]; 
+
+function greetings() {
+  log(chalk.white.bold("--------------------🌸🌸🌸--------------------"));
+  log(chalk.white.bold("Welcome to the quiz, Let's play!!"));
+  log(chalk.white.bold("--------------------🌸🌸🌸--------------------"));
+};
+greetings();
+
+function startQuiz() {
+  userName = readlineSync.question(chalk.white.underline.bold("Please enter your name") + ":" + " ");
+  if(userName === "") {
+    log(chalk.bgRed.bold("Invalid entry, Please enter your name"));
+    startQuiz();
+  } else {
+    log(chalk.white.bold("--------------------💮💮💮--------------------"));
+    log(chalk.bgWhite.bold(`Let's play the quiz ${userName}`));
+    log(chalk.white.bold("--------------------💮💮💮--------------------"));
+    exitFunc();
+  }
+};
+startQuiz();
+
+function exitFunc() {
+  const continueOrExit = readlineSync.question(chalk.white.underline.bold("Do you want to continue [y/n]") + ":" + " ");
+  if(continueOrExit === "n") {
+    log(chalk.white.bold("--------------------💔💔💔--------------------"));
+    log(chalk.bgWhite.bold(`Noooo it's too soon ${userName}, Comeback any time.`));
+    log(chalk.white.bold("--------------------💔💔💔--------------------"));
+  } else if(continueOrExit === "y") {
+    quiz();
+  } else {
+    log(chalk.bgRed.bold("Invalid entry, Please enter [y/n] to continue"));
+    exitFunc();
+  }
+}
+
+function quiz() {
+  for(let i = 0; i < gkQuiz.length; i++) {
+    const currentQuestion = gkQuiz[i].question;
+    const currentCorrectAnswer = gkQuiz[i].correctAnswer;
+    const userInput = readlineSync.question(chalk.white.bold(currentQuestion) + chalk.white.bold.underline("\nAnswer") + ":" + " ");
+    if (options.includes(userInput)) {
+      if (userInput === currentCorrectAnswer) {
+        log(chalk.bgGreen.bold("Right Answer"));
+        score += 2;
+      } else {
+        log(chalk.bgRed.bold("Wrong Answer"));
+        const showRightAnswer = chalk.white.bold.underline("Correct answer") + ":" + " " + chalk.bgGreen.bold(currentCorrectAnswer);
+        log(showRightAnswer);
+        score -= 1;
+      };
+    } else {
+      log(chalk.bgRed.bold("Invalid entry, Please enter valid option"));
+      i--;
+    };
+  }
+  endQuiz();
+};
+
+function endQuiz() {
+  log(chalk.white.bold("--------------------✨✨✨--------------------"));
+  log(chalk.bgYellow.bold("Score:", score));
+  log(chalk.bgWhite.bold(`Thanks ${userName} for playing quiz.`));
+  log(chalk.white.bold("--------------------✨✨✨--------------------"));
+};
---
+++ dataBase.js
@@ -0,0 +1,84 @@
+const gkQuiz = [
+    {
+      question: `What is the largest planet in our solar system?
+        a: Earth
+        b: Jupiter
+        c: Mars
+        d: Venus`,
+      correctAnswer: "b",
+    },
+    {
+      question: `Who wrote 'Romeo and Juliet'?
+        a: Charles Dickens
+        b: Jane Austen
+        c: William Shakespeare
+        d: Mark Twain`,
+      correctAnswer: "c",
+    },
+    {
+      question: `What is the capital of Japan?
+        a: Tokyo
+        b: Beijing
+        c: Seoul
+        d: Bangkok`,
+      correctAnswer: "a",
+    },
+    {
+      question: `Which planet is known as the Red Planet?
+        a: Venus
+        b: Mercury
+        c: Jupiter
+        d: Mars`,
+      correctAnswer: "d",
+    },
+    {
+      question: `Who painted the Mona Lisa?
+        a: Vincent van Gogh
+        b: Leonardo da Vinci
+        c: Pablo Picasso
+        d: Claude Monet`,
+      correctAnswer: "b",
+    },
+    {
+      question: `What is the currency of Australia?
+        a: Euro
+        b: Yen
+        c: Dollar
+        d: Pound`,
+      correctAnswer: "c",
+    },
+    {
+      question: `In which year did the Titanic sink?
+        a: 1912
+        b: 1920
+        c: 1905
+        d: 1931`,
+      correctAnswer: "a",
+    },
+    {
+      question: `Who developed the theory of relativity?
+        a: Isaac Newton
+        b: Galileo Galilei
+        c: Stephen Hawking
+        d: Albert Einstein`,
+      correctAnswer: "d",
+    },
+    {
+      question: `What is the capital of Brazil?
+        a: Rio de Janeiro
+        b: Brasília
+        c: São Paulo
+        d: Buenos Aires`,
+      correctAnswer: "b",
+    },
+    {
+      question: `Which element has the chemical symbol 'O'?
+        a: Oxygen
+        b: Gold
+        c: Silver
+        d: Iron`,
+      correctAnswer: "a",
+    },
+];
+
+module.exports = gkQuiz;
